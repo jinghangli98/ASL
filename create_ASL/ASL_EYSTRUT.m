87553877c70bccb191ee0a01139723fe3e7e46f3
jinghangli98 <jil202@pitt.edu>
2022-12-20 15:49:10 -0500

    rotated ASL_EYSTRUT

--- create_ASL/ASL_EYSTRUT.m
+++ create_ASL/ASL_EYSTRUT.m
@@ -2,28 +2,49 @@ clear;
 close all;
 clc;
 
-EYSTRUT = load('./nonrotated/geometry/EYSTRUT.dat');
-moving = EYSTRUT;
+EYSTRUT = load('../nonrotated/geometry/EYSTRUT.dat');
+EYSTRUT_moving = EYSTRUT;
 
-moving(find(moving(:,2) <= 160), :) = [];
-moving(find(moving(:,3) <= 162), :) = [];
+% moving(find(moving(:,2) <= 160), :) = [];
+
+EYSTRUT_moving(find(EYSTRUT_moving(:,3) >= 162), :) = [];
+moving = zeros(max(EYSTRUT_moving(:,1)), max(EYSTRUT_moving(:,2)), max(EYSTRUT_moving(:,3)));
+
+for i=1:size(EYSTRUT_moving,1)
+    moving(EYSTRUT_moving(i,1), EYSTRUT_moving(i,2), EYSTRUT_moving(i,3)) = 1;
+end
 
 
+eulerAngles = [0 0 180];
+trans = [0 0 0];
+tform = rigidtform3d(eulerAngles, trans);
 
-middle_piece = moving(find(moving(:,1) <= 160 & moving(:,1) >= 95), :);
-middle_piece(:,3) = middle_piece(:,3) - 7;
+Rin = imref3d(size(moving));
+Rin.XWorldLimits = Rin.XWorldLimits-mean(Rin.XWorldLimits);
+Rin.YWorldLimits = Rin.YWorldLimits-mean(Rin.YWorldLimits);
+Rin.ZWorldLimits = Rin.ZWorldLimits-mean(Rin.ZWorldLimits);
+rotated = imwarp(moving, Rin, tform);
 
-moving(find(moving(:,1) <= 160 & moving(:,1) >= 95), :) = [];
+[x,y,z] = ind2sub(size(rotated), find(rotated ~=0));
+rotated = [x + (37-1) , y + (218 - 176) , z- (76 + 6)];
+rotated(find(rotated(:,2) <= 161 ),:) = [];
 
-moving = [moving; middle_piece];
-plot3d(moving)
 
-move_z = 72 + 72 + 6; 
-moving(:,3) = moving(:,3) - move_z;
+% 
+% middle_piece = moving(find(moving(:,1) <= 160 & moving(:,1) >= 95), :);
+% middle_piece(:,3) = middle_piece(:,3) - 7;
+% 
+% moving(find(moving(:,1) <= 160 & moving(:,1) >= 95), :) = [];
+% 
+% moving = [moving; middle_piece];
+% plot3d(moving)
+% 
+% move_z = 72 + 72 + 6; 
+% moving(:,3) = moving(:,3) - move_z;
 
-EYSTRUT = [EYSTRUT; moving];
+EYSTRUT = [EYSTRUT; rotated];
 
 plot3d(EYSTRUT)
 
-writematrix(EYSTRUT, './geometry/ASL_EYSTRUT.dat', 'Delimiter', ' ')
+writematrix(EYSTRUT, '../geometry/ASL_EYSTRUT.dat', 'Delimiter', ' ')
 
